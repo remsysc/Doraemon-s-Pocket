e68e3c1e132d6225fc5cc4bc181da39777c25d0f
remsysc <raipam44@gmail.com>
2026-07-22 15:03:37 +0800

    Define role accces on routes api

--- routes/api.php
+++ routes/api.php
@@ -8,33 +8,34 @@ use App\Http\Controllers\LotController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-//use protected routes
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-    Route::get('/user', fn(Request $request) => $request->user());
+    Route::get('/user', fn (Request $request) => $request->user());
 
-    // Category CRUD
+    // ---- Read access: any authenticated role ----
+    // Everyone needs to browse the catalog to do their job (picking, ordering, reporting).
     Route::get('/categories', [CategoryController::class, 'index']);
-    Route::post('/categories', [CategoryController::class, 'store']);
     Route::get('/categories/{category}', [CategoryController::class, 'show']);
-    Route::put('/categories/{category}', [CategoryController::class, 'update']);
-    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
-
-    // Product CRUD
     Route::get('/products', [ProductController::class, 'index']);
-    Route::post('/products', [ProductController::class, 'store']);
     Route::get('/products/{product}', [ProductController::class, 'show']);
-    Route::put('/products/{product}', [ProductController::class, 'update']);
-    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
-
-    // Lot CRUD
     Route::get('/lots', [LotController::class, 'index']);
-    Route::post('/lots', [LotController::class, 'store']);
     Route::get('/lots/{lot}', [LotController::class, 'show']);
-    Route::put('/lots/{lot}', [LotController::class, 'update']);
-    Route::delete('/lots/{lot}', [LotController::class, 'destroy']);
+
+    // ---- Purchasing Manager + Admin: catalog & procurement writes ----
+    Route::middleware('role:admin,purchasing_manager')->group(function () {
+        Route::post('/categories', [CategoryController::class, 'store']);
+        Route::put('/categories/{category}', [CategoryController::class, 'update']);
+        Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
+
+        Route::post('/products', [ProductController::class, 'store']);
+        Route::put('/products/{product}', [ProductController::class, 'update']);
+        Route::delete('/products/{product}', [ProductController::class, 'destroy']);
+
+        Route::post('/lots', [LotController::class, 'store']);
+        Route::put('/lots/{lot}', [LotController::class, 'update']);
+        Route::delete('/lots/{lot}', [LotController::class, 'destroy']);
+    });
 });
